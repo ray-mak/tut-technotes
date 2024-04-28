8d275ea5a7ab40660672e7ae0b749d119615daff
Ray Mak <projectraymak@gmail.com>
2024-04-28 15:53:57 -0700

    Created React Router

---
+++ client/src/features/notes/NotesList.jsx
@@ -0,0 +1,5 @@
+export default function NotesList() {
+    return (
+        <h1>NotesList</h1>
+    )
+}
---
+++ client/src/features/users/UsersList.jsx
@@ -0,0 +1,5 @@
+export default function UsersList() {
+    return (
+        <h1>UsersList</h1>
+    )
+}
